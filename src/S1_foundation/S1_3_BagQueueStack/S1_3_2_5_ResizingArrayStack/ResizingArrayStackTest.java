1ad7ed61b01406b3d1461adf53465393f09f5316
BrianLee <brianleelxt@gmail.com>
2018-08-07 18:45:49 +0800

    add ResizingArrayStack

---
+++ src/S1_foundation/S1_3_BagQueueStack/S1_3_2_5_ResizingArrayStack/ResizingArrayStackTest.java
@@ -0,0 +1,24 @@
+package S1_foundation.S1_3_BagQueueStack.S1_3_2_5_ResizingArrayStack;
+import java.util.Scanner;
+
+/**
+ * Created by Brian Lee on 2018/7/30.
+ */
+public class ResizingArrayStackTest {
+    public static void main(String[] args) {
+        ResizingArrayStack<String> s = new ResizingArrayStack<>();
+        Scanner in = new Scanner(System.in);
+        String str = in.nextLine();
+        String strArray[] = str.split(" ");
+        for (int i = 0; i < strArray.length; i++) {
+            String ch = strArray[i];
+            if (!ch.equals("-")) {
+                s.push(ch);
+            }
+            else if (!s.isEmpty()) {
+                System.out.printf(s.pop() + " ");
+            }
+        }
+        System.out.println("\n" + s.size() + " left on stack");
+    }
+}
