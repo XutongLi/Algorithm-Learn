31c61cd72d9dab27f13e0d5947575f1b45f99c38
BrianLee <brianleelxt@gmail.com>
2018-08-11 00:08:05 +0800

    add Insertion sort

---
+++ src/S2_Sorting/S2_1_3_Insertion_Sort/Insertion.java
@@ -0,0 +1,49 @@
+package S2_Sorting.S2_1_3_Insertion_Sort;
+
+import java.util.Scanner;
+
+/**
+ * Created by Brian Lee on 2018/8/10.
+ */
+public class Insertion {
+    public static void sort(Comparable[] a) {
+        int N = a.length;
+        for (int i = 1; i < N; i++) {
+            for (int j = i; j > 0 && less(a[j], a[j - 1]); j--)
+                exch(a, j , j - 1);
+        }
+    }
+
+    private static boolean less(Comparable v, Comparable w) {
+        return v.compareTo(w) < 0;
+    }
+
+    private static void exch(Comparable[] a, int i, int j) {
+        Comparable t = a[i];
+        a[i] = a[j];
+        a[j] = t;
+    }
+
+    private static void show(Comparable[] a) {
+        for (int i = 0; i < a.length; i++) {
+            System.out.printf(a[i] + " ");
+        }
+    }
+
+    public static boolean isSorted(Comparable[] a) {
+        for (int i = 1; i < a.length; i++) {
+            if (less(a[i], a[i - 1]))
+                return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String str = in.nextLine();
+        String[] a = str.split(" ");
+        sort(a);
+        assert isSorted(a);
+        show(a);
+    }
+}
