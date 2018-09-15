1299340a97d79eff3b6f12d4fea8978ea265b84c
BrianLee <brianleelxt@gmail.com>
2018-09-16 00:14:32 +0800

    add DiGraph

---
+++ src/S4_Graphs/S4_2_Directed_Graph/S4_2_2_DiGraph/DiGraphTest.java
@@ -0,0 +1,34 @@
+package S4_Graphs.S4_2_Directed_Graph.S4_2_2_DiGraph;
+
+/**
+ * Created by Brian Lee on 2018/9/16.
+ 13 22
+ 4 2
+ 2 3
+ 3 2
+ 6 0
+ 0 1
+ 2 0
+ 11 12
+ 12 9
+ 9 10
+ 9 11
+ 8 9
+ 10 12
+ 11 4
+ 4 3
+ 3 5
+ 7 8
+ 8 7
+ 5 4
+ 0 5
+ 6 4
+ 6 9
+ 7 6
+ */
+public class DiGraphTest {
+    public static void main(String[] args) {
+        DiGraph g = new DiGraph();
+        System.out.println(g);
+    }
+}
