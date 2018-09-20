cb61b948f69cfad4dc9e350ed39e59f8215f6f72
BrianLee <brianleelxt@gmail.com>
2018-09-20 18:42:14 +0800

    add di DFS path

--- src/S4_Graphs/S4_2_Directed_Graph/S4_2_3_DiDFSPath/DiDFSPath.java
+++ src/S4_Graphs/S4_2_Directed_Graph/S4_2_3_DiDFSPath/DiDFSPath.java
@@ -1,8 +1,48 @@
 package S4_Graphs.S4_2_Directed_Graph.S4_2_3_DiDFSPath;
 
+import S4_Graphs.S4_2_Directed_Graph.S4_2_2_DiGraph.DiGraph;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
 /**
  * Created by Brian Lee on 2018/9/20.
  */
 public class DiDFSPath {
-    
+    private boolean[] marked;
+    private int[] edgeTo;
+    private int s;
+
+    public DiDFSPath(DiGraph G, int s) {
+        marked = new boolean[G.V()];
+        edgeTo = new int[G.V()];
+        this.s = s;
+        dfs(G, s);
+    }
+    private void dfs(DiGraph G, int v) {
+        marked[v] = true;
+        for (int w : G.adj(v)) {
+            if (!marked[w]) {
+                edgeTo[w] = v;
+                dfs(G, w);
+            }
+        }
+    }
+    public boolean hasPathTo(int v) {
+        return marked[v];
+    }
+    public Iterable<Integer> pathTo(int v) {
+        if (!hasPathTo(v))   return null;
+        Stack<Integer> stack = new Stack<>();
+        for (int x = v; x != s; x = edgeTo[x]) {
+            stack.push(x);
+        }
+        stack.push(s);
+        Queue<Integer> queue = new LinkedList<>();
+        while (!stack.isEmpty()) {
+            queue.offer(stack.pop());
+        }
+        return queue;
+    }
 }
